6d055bc307d32c6f8294282ca6165c6493bf70f1
pavel <p.baranushkin@gmail.com>
2023-10-16 13:22:49 +0400

    refactor: problem 88

--- 88_merge_sorted_array/problem88.go
+++ 88_merge_sorted_array/problem88.go
@@ -1,25 +1,15 @@
 package problem88
 
 func merge(nums1 []int, m int, nums2 []int, n int) {
-	n1, n2, w, leftIsOver := m-1, n-1, len(nums1)-1, false
-
-	if n1 < 0 {
-		n1 = 0
-		leftIsOver = true
-	}
-
+	n1, n2, w := m-1, n-1, len(nums1)-1
 	for n2 >= 0 {
-		if nums2[n2] >= nums1[n1] || leftIsOver {
+		if n1 < 0 || nums2[n2] >= nums1[n1] {
 			nums1[w] = nums2[n2]
 			n2--
 		} else {
 			nums1[w], nums1[n1] = nums1[n1], nums1[w]
 			n1--
 		}
-
 		w--
-		if leftIsOver || n1 < 0 {
-			n1, leftIsOver = w, true
-		}
 	}
 }
